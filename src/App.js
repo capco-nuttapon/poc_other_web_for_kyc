a9f97058c6879fb0a6e7ec0e69964c57f47673df
Nuttapon Buaban <145950333+capco-nuttapon@users.noreply.github.com>
2024-03-12 11:23:36 +0700

    feat: add go to KYC callback

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
-import logo from "./logo.svg";
+import logo from "./logo_capco.png";
 import "./App.css";
+import "./tailwind.css";
+
+function getMobileOperatingSystem() {
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+  // Windows Phone must come first because its UA also contains "Android"
+  if (/windows phone/i.test(userAgent)) {
+    return "Windows Phone";
+  }
+
+  if (/android/i.test(userAgent)) {
+    return "Android";
+  }
+
+  // iOS detection from: http://stackoverflow.com/a/9039885/177710
+  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    return "iOS";
+  }
+
+  return "unknown";
+}
 
 function App() {
+  function onKYCClicked() {
+    if (getMobileOperatingSystem() === "Android") {
+      if (window.kyc !== undefined) {
+        window.kyc.onKYCClicked("");
+      }
+    } else if (getMobileOperatingSystem() === "iOS") {
+      if (window.webkit !== undefined) {
+        window.webkit.messageHandlers.onKYCClicked.postMessage("");
+      }
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Just another web app</p>
-        <a
-          className="App-link"
-          href="https://capco-nuttapon.github.io/poc_kyc_web/#/?isPush=true"
-        >
-          Push to KYC
-        </a>
+        <img src={logo} className="App-logo mb-24" alt="logo" />
+        <p className="text-black">Capco Web app</p>
+        <button className="App-link" onClick={onKYCClicked}>
+          Go to KYC
+        </button>
       </header>
     </div>
   );
